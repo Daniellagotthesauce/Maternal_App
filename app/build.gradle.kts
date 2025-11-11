0772fe59844b59ee25872b949bcbc58971d14d71
FChelulei <faith.chelulei@strathmore.edu>
2025-11-11 23:22:04 +0300

    Google fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-    //id("com.google.gms.google-services")
+    id("com.google.gms.google-services")
 }
 
 android {
